0bd9d230fd3f2d3a78e62110524e2ce1e72000c3
Joseph Wu <78169587+WazzaPd@users.noreply.github.com>
2023-05-25 15:34:45 -0400

    reorganize

--- box/app/__init__.py
+++ box/app/__init__.py
@@ -1,11 +1,35 @@
-from flask import Flask
-app=Flask(__name__)
+# import sqlite3  # for database building
+import sqlite3
+
+from flask import Flask # facilitate flask webserving
+from flask import render_template  # facilitate jinja templating
+from flask import request  # facilitate form submission
+from flask import session  # facilitate user sessions
+
+app=Flask(__name__) # create Flask object
 # DO NOT EDIT ABOVE
 
 @app.route("/")
-def hello():
-        return "Hello World"
+def index():
+        return render_template("index.html")
+
+@app.route("/battering_ram")
+def battering_ram():
+        return render_template("battering_ram.html")
+
+@app.route("/cluster_bomb")
+def cluster_bomb():
+        return render_template("cluster_bomb.html")
+
+@app.route("/pitchfork")
+def pitchfork():
+        return render_template("pitchfork.html")
+
+@app.route("/sniper")
+def sniper():
+        return render_template("sniper.html")
+
 
 # DO NOT EDIT BELOW
 if __name__ == "__main__":
-        app.run()
+app.run()
